e13033f4212bb988e31eacd1fe2e890464f4931c
viniciusfcar <viniciuscarneiro007@gmail.com>
2020-11-29 20:22:46 -0300

    Commit Inicial

--- accounts/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import login
-from .views import logout
-
-urlpatterns = [
-    path('login/', login, name='login'),
-    path('logout/', logout, name='logout'),
-]
